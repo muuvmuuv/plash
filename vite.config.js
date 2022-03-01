5995ebb555811beb9cddf335ef2cff182dfce5cf
Marvin Heilemann <11534760+muuvmuuv@users.noreply.github.com>
2022-03-01 10:45:45 +0100

    Add Inter font

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
+const isProd = process.env.NODE_ENV === "production"
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/plash/" : "/",
+  base: isProd ? "/plash/" : "/",
   plugins: [vue()],
 })
