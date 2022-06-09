dcb17df1a6d80558ff293b848cb594c998415b26
Marvin Heilemann <11534760+muuvmuuv@users.noreply.github.com>
2022-06-09 13:39:57 +0200

    Add stuff

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { computed, onMounted, ref } from "vue"
 import dayjs from "dayjs"
+import Fairy from "./components/Fairy.vue"
+import Dots from "./components/Dots.vue"
+import Portal from "./components/Portal.vue"
 
 const date = ref(dayjs())
 const formated = computed(() => date.value.format("LLLL:ss"))
@@ -17,6 +20,8 @@ onMounted(() => {
     <h1 class="text-4xl tabular-nums text-slate-50 opacity-40">{{ formated }}</h1>
   </header>
   <main>
-    <!-- <section></section> -->
+    <Fairy class="absolute bottom-48 left-64"></Fairy>
+    <Dots></Dots>
+    <Portal class="absolute bottom-12 right-8" :width="300" :height="300"></Portal>
   </main>
 </template>
