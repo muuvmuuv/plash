99df32b3d8cbf9b8347119052f7859b77cc69fee
Marvin Heilemann <11534760+muuvmuuv@users.noreply.github.com>
2023-01-05 17:06:41 +0100

    Use video as background

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ My [Plash](https://sindresorhus.com/plash) wallpaper/background.
 - Install with `pnpm install`
 - Build `pnpm build`
 - Run container `just run`
+
+## Resources
+
+Get videos from Steam using https://steamworkshopdownloader.io/ and
+[SteamCMD](https://developer.valvesoftware.com/wiki/SteamCMD#macOS).
+
+- Wallpaper Engine: https://steamcommunity.com/app/431960/workshop/
--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <script setup>
-import Morphing from "./components/morphing/Morphing.vue"
 import Weather from "./components/Weather.vue"
 </script>
 
 <template>
+  <video src="/cyberpunk.mp4" autoplay muted :controls="false" loop></video>
+
   <main class="grid grid-rows-6 grid-cols-12 gap-4 w-full h-full p-6">
     <section class="col-span-3 row-span-2">
       <Weather class="ml-2"></Weather>
     </section>
   </main>
-
-  <Morphing class="absolute top-0 left-0 -z-10"></Morphing>
 </template>
+
+<style scoped>
+video {
+  position: fixed;
+  top: 0;
+  left: 0;
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+</style>
